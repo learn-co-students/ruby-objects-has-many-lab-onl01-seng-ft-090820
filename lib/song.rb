9c92f617ae73f4a72c0b14d6191430ce85ea2baa
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-27 19:55:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,18 @@
-class Song
-  attr_accessor :name, :genre, :artist
+class Song	
+  attr_accessor :name, :artist	
 
-  @@all = []
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+  @@all = []	
 
-  def self.all
-    @@all
-  end
+  def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
 
-  def artist_name
-    if artist == nil
-      nil
-    else
-      self.artist.name
-    end
-  end
+  def self.all	
+    @@all	
+  end	
+
+  def artist_name	
+    artist.name if artist	
+  end	
 end
