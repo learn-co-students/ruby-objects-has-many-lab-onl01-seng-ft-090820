26071b93eb4212c0a9d8fdddfd10aea887f702e5
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-27 19:52:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,26 +1,25 @@
-class Author 
-  attr_accessor :posts
-  
-  @@all = []
-  
+class Author
+  attr_accessor :name
+
   def initialize(name)
     @name = name
-    @@all << self 
   end
-  
+
+  def posts
+    Post.all
+  end
+
   def add_post(post)
     post.author = self
   end
-  
+
   def add_post_by_title(title)
-    post = post.new(title)
-    post.author = self
-  end 
-  
+    post = Post.new(title)
+    add_post(post)
+  end
+
   def self.post_count
-    post.all.count
-  end 
+    Post.all.count
+  end
+
 end
-    
-
-
