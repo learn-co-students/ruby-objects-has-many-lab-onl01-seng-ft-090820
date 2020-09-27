9a33b9832d67dd4b195b6c99045452f275153c22
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-27 19:52:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,25 @@
-class Post
-  attr_accessor :title, :author
+class Artist
+  attr_accessor :name
 
-  @@all = []
-
-  def initialize (title)
-    @title = title
-    @@all << self
+  def initialize(name)
+    @name = name
   end
 
-  def self.all
-    @@all
+  def add_song(song)
+    song.artist = self
   end
 
-  def author_name
-    if author == nil
-      nil
-    else
-      self.author.name
-    end
+  def songs
+     Song.all.select {|song| song.artist == self}
   end
 
-  def add_post(post)
-    post.author = self
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
+  def self.song_count
+    Song.all.count
   end
 
 end
